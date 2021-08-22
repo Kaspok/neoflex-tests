0e3fff333f2510b26c8f87e4c07f844b0274938d
Kaspok <kaspok251198u@yandex.ru>
2021-08-22 18:34:47 +0300

    Добавил тест на проверку контактов в нижней части страницы

--- src/test/java/ru/neoflex/pages/GeneralsBlocks.java
+++ src/test/java/ru/neoflex/pages/GeneralsBlocks.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import ru.neoflex.data.MenuItem;
 
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -24,6 +25,12 @@ public class GeneralsBlocks {
     private SelenideElement languageWidget = allMenuItems.$(By.linkText(MenuItem.EN.getDisplayName()));
     private SelenideElement searchWidget = allMenuItems.$(".search");
 
+    private SelenideElement allFooterContacts = $(".footer");
+    private SelenideElement leftFooterContacts = allFooterContacts.$(".footer__contacts-wrapper");
+    private SelenideElement centerFooterContacts = allFooterContacts.$(".footer__center");
+    private SelenideElement rightFooterContacts = allFooterContacts.$(".footer__ppd");
+
+
     @Step("Проверяем, что все виджеты на верхней панели отображаются")
     public void checkThatWidgetsDisplayed() {
         solutionsWidget.shouldBe(visible);
@@ -37,5 +44,33 @@ public class GeneralsBlocks {
         searchWidget.shouldBe(visible);
     }
 
+    @Step("Проверяем контакты в нижней части страницы")
+    public void checkFooterContacts() {
+        leftFooterContacts.$(".footer__support").scrollTo()
+                .shouldHave(exactTextCaseSensitive("Служба технической поддержки"));
+        leftFooterContacts.$(".support-phone")
+                .shouldHave(exactTextCaseSensitive("+7 (800) 100 77 28"));
+        leftFooterContacts.$(".support-time")
+                .shouldHave(exactTextCaseSensitive("круглосуточно"));
+        leftFooterContacts.$(".copyright")
+                .shouldHave(exactTextCaseSensitive("© 2021 Neoflex. Все права защищены"));
+
+        centerFooterContacts.$(".base-button")
+                .shouldHave(exactTextCaseSensitive("Связаться с нами"));
+        centerFooterContacts.$(".footer__phone")
+                .shouldHave(exactTextCaseSensitive("+7 (495) 984 25 13"));
+        centerFooterContacts.$(".footer__email")
+                .shouldHave(exactTextCaseSensitive("info@neoflex.ru"));
+        centerFooterContacts.$(".social-link").shouldBe(visible);
+
+        rightFooterContacts.$(".address-title")
+                .shouldHave(exactTextCaseSensitive("Адрес"));
+        rightFooterContacts.$(".footer__address")
+                .shouldHave(exactTextCaseSensitive("127015, Москва, ул. Вятская, д. 35, стр. 4, 1 подъезд, 2 этаж"));
+        rightFooterContacts.$(".policy-ppd")
+                .shouldHave(exactTextCaseSensitive("Политика обработки персональных данных"));
+        rightFooterContacts.$(".consent-ppd")
+                .shouldHave(exactTextCaseSensitive("Согласие на обработку персональных данных"));
+    }
 
 }
--- src/test/java/ru/neoflex/tests/HomePageTests.java
+++ src/test/java/ru/neoflex/tests/HomePageTests.java
@@ -3,7 +3,10 @@ package ru.neoflex.tests;
 import org.junit.jupiter.api.Test;
 import ru.neoflex.pages.GeneralsBlocks;
 
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class HomePageTests extends TestBase {
 
@@ -11,8 +14,18 @@ public class HomePageTests extends TestBase {
 
     @Test
     void homePageShouldContains9Widgets() {
+        final String homeGreeting = "Помогаем бизнесу получать устойчивые конкурентные преимущества в цифровую эпоху";
         open("");
+        step("Проверяем, что верхнее описание содержит текст: Помогаем бизнесу получать устойчивые " +
+                "конкурентные преимущества в цифровую эпоху", () -> {
+            $(".home-greeting").$(".home-greeting__title").shouldHave(exactTextCaseSensitive(homeGreeting));
+        });
         generalsBlocks.checkThatWidgetsDisplayed();
+    }
 
+    @Test
+    void checkFooterContacts() {
+        open("");
+        generalsBlocks.checkFooterContacts();
     }
 }
